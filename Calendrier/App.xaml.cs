07fc2472a147c364217c8e99b4a95491f0ba4d06
Vincent Tam <sere@live.hk>
2020-10-09 11:52:39 +0200

    enveler la méthode de démarrage

--- Calendrier/App.xaml.cs
+++ Calendrier/App.xaml.cs
@@ -13,9 +13,5 @@ namespace Calendrier
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            var vm = new Calendrier.ViewModel.CalendrierVM();
-        }
     }
 }

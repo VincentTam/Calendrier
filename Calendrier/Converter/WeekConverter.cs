341cc3561dc2933a2eca933c180e182386c4cf23
Vincent Tam <sere@live.hk>
2020-11-02 17:50:21 +0100

    sect 4,4,3 créa'n du convertisseur

---
+++ Calendrier/Converter/WeekConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Calendrier.Converter
+{
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    class WeekConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
